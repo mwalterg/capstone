5ecfde52e967689cacb5229fd4b302576b061e13
Marvin Gill <marvg@Marvins-MBP.lan>
2023-09-28 16:49:51 -0400

    added remove and ability to adjust qty

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { useCart } from '../components/CartContext';
 
 function CartPage() {
-  const { cart } = useCart(); // Access the cart state from the CartContext
+  const { cart, updateCartItemQuantity, removeFromCart } = useCart();
+
+  const handleQuantityChange = (itemId, newQuantity) => {
+    updateCartItemQuantity(itemId, newQuantity);
+  };
+
+  const handleRemoveItem = (itemId) => {
+    removeFromCart(itemId);
+  };
 
   // Calculate the total price of items in the cart
-  const total = cart.reduce((acc, item) => acc + item.price, 0);
+  const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
 
   return (
     <div>
@@ -13,7 +21,14 @@ function CartPage() {
       <ul>
         {cart.map((item) => (
           <li key={item.id}>
-            {item.title} - ${item.price}
+            {item.title} - ${item.price} x{' '}
+            <input
+              type="number"
+              value={item.quantity}
+              onChange={(e) => handleQuantityChange(item.id, parseInt(e.target.value))}
+              min="1"
+            />{' '}
+            <button onClick={() => handleRemoveItem(item.id)}>Remove</button>
           </li>
         ))}
       </ul>
@@ -23,3 +38,4 @@ function CartPage() {
 }
 
 export default CartPage;
+
