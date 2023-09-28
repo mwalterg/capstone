edd8faf7afa1608c8dd74b864c01b8ba938a7b01
Marvin Gill <marvg@Marvins-MBP.lan>
2023-09-28 12:38:21 -0400

    cart working

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ProductListPage from './pages/ProductListPage';
 import ProductDetailsPage from './pages/ProductDetailsPage';
-import CartPage from './pages/CartPage'; // Import the CartPage
+import CartPage from './pages/CartPage';
 import Navbar from './components/Navbar';
-import { CartProvider } from './components/CartContext'; // Import the CartProvider
+import { CartProvider } from './components/CartContext';
 
 function App() {
   return (
-    <CartProvider> {/* Wrap your app with CartProvider */}
+    <CartProvider> 
       <Navbar />
       <Routes>
         <Route exact path="/" element={<ProductListPage />} />
         <Route path="/product/:id" element={<ProductDetailsPage />} />
-        <Route path="/cart" element={<CartPage />} /> {/* Add a route for the CartPage */}
+        <Route path="/cart" element={<CartPage />} /> 
         {/*<Route path="/checkout" element={<CheckoutPage />} />*/}
       </Routes>
     </CartProvider>
